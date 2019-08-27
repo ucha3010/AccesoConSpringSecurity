46cd9128a54bf39a5915495f02e2667185eeadff
Damian <damianjava@gmail.com>
2019-08-27 21:00:18 +0200

    BBDD actualizada

--- WebContent/WEB-INF/utils/springbd.sql
+++ WebContent/WEB-INF/utils/springbd.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-08-2019 a las 15:12:56
--- Versión del servidor: 10.3.16-MariaDB
--- Versión de PHP: 7.1.30
+-- Tiempo de generación: 27-08-2019 a las 20:59:24
+-- Versión del servidor: 10.1.13-MariaDB
+-- Versión de PHP: 5.6.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -80,7 +78,11 @@ INSERT INTO `datospersonales` (`idDatosPers`, `nombre`, `apellido`, `sexo`, `fec
 (1, 'José', 'Martínez', 'Masculino', '1967-07-11', 'España', '123456789A', 'pepe@pepe.com', 1),
 (2, 'Juan', 'Álvarez', 'masculino', '1984-10-24', 'España', '123456789B', 'juan@juan.com', 2),
 (3, 'Roberto', 'Do Santos', 'masculino', '1980-07-28', 'Portugal', '123456789C', 'roberto@roberto.com', 3),
-(4, 'Damián', 'Usheff', 'masculino', '1976-10-30', 'Argentina', '123456789D', 'damian@damian.com', 4);
+(4, 'Damián', 'Usheff', 'masculino', '1976-10-30', 'Argentina', '123456789D', 'damian@damian.com', 4),
+(5, 'Carlos Alberto', 'Velazquez de la Tarragona', 'masculino', '1975-08-01', 'España', '123456789E', 'carlos.alberto.velazquez.delatarragona@telefonicaladron.com', 5),
+(13, 'Alexandra', 'Huertas', 'femenino', '1994-08-22', 'España', '123456789M', 'alex@gmail.com', 13),
+(14, 'Alicia', 'Cruz', 'femenino', '2005-10-27', 'España', '123456789N', 'ali@gmail.com', 14),
+(15, 'Miguel Ángel', 'Cruz', 'masculino', '2010-10-27', 'España', '123456789O', 'miki@gmail.com', 15);
 
 -- --------------------------------------------------------
 
@@ -152,13 +154,16 @@ INSERT INTO `usuario` (`idUsr`, `usuario`, `clave`, `habilitado`, `fechaCreacion
 (2, 'juan', '44e31399fa4840954f1f7b8e14b96052e3622019c8ab4856a62f3d4926cbd645f8206d3884a3d173', 1, '2018-08-12 00:00:00', 2),
 (3, 'roberto', '44e31399fa4840954f1f7b8e14b96052e3622019c8ab4856a62f3d4926cbd645f8206d3884a3d173', 1, '2018-08-13 18:35:04', 3),
 (4, 'Damian', '44e31399fa4840954f1f7b8e14b96052e3622019c8ab4856a62f3d4926cbd645f8206d3884a3d173', 1, '2018-08-13 18:35:35', 4),
-(5, 'Carlos', '04552a2c746630ab679e38cbfb8545dd8698ef0bba56dfc116f043fc4a35eb735bcdcf04b6fed81b', 1, '2018-08-15 20:32:19', 5),
-(7, 'Francisco', '08c8e900d01569347f69100921d34e6973825d474d8c74f0d1ff906a22b5e888622619bc88cbf691', 1, '2018-08-15 22:20:08', 7),
-(8, 'bernardo', '1234', 1, '2019-03-04 22:36:10', 8),
-(9, 'sebastian', '69251d916e553a69cbfb4a3732aaa109030002321590e62cf642bebb3b691b4ffd8c08dfa1b98262', 1, '2019-03-04 22:43:03', 9),
-(10, 'Juana', '2141963b424538d4fe4fbb2fdac8b11c3a47616f472d7646d648a18c3477e0ebc4bce137b501e073', 1, '2019-03-07 23:12:47', 10),
-(11, 'Eduardo', '8f7f2fbad65eacc6fcfac17d158c735fc21e8a87a4cb86b9d98a64d65730834a005892dc31aad362', 1, '2019-03-07 23:27:42', 11),
-(12, 'Manuel', '858df5b4059e3bf5d362c0c84f1b9d9ab9ea79f493a92e851575f430b8d49b0300fa602934b3ec89', 1, '2019-08-07 22:59:02', 12);
+(5, 'Carlos', '44e31399fa4840954f1f7b8e14b96052e3622019c8ab4856a62f3d4926cbd645f8206d3884a3d173', 1, '2018-08-15 20:32:19', 5),
+(7, 'Francisco', '44e31399fa4840954f1f7b8e14b96052e3622019c8ab4856a62f3d4926cbd645f8206d3884a3d173', 1, '2018-08-15 22:20:08', 7),
+(8, 'bernardo', '44e31399fa4840954f1f7b8e14b96052e3622019c8ab4856a62f3d4926cbd645f8206d3884a3d173', 1, '2019-03-04 22:36:10', 8),
+(9, 'sebastian', '44e31399fa4840954f1f7b8e14b96052e3622019c8ab4856a62f3d4926cbd645f8206d3884a3d173', 1, '2019-03-04 22:43:03', 9),
+(10, 'Juana', '44e31399fa4840954f1f7b8e14b96052e3622019c8ab4856a62f3d4926cbd645f8206d3884a3d173', 1, '2019-03-07 23:12:47', 10),
+(11, 'Eduardo', '44e31399fa4840954f1f7b8e14b96052e3622019c8ab4856a62f3d4926cbd645f8206d3884a3d173', 1, '2019-03-07 23:27:42', 11),
+(12, 'Manuel', '44e31399fa4840954f1f7b8e14b96052e3622019c8ab4856a62f3d4926cbd645f8206d3884a3d173', 1, '2019-08-07 22:59:02', 12),
+(13, 'Alexandra', '44e31399fa4840954f1f7b8e14b96052e3622019c8ab4856a62f3d4926cbd645f8206d3884a3d173', 1, '2019-08-27 00:00:00', 13),
+(14, 'Alicia', '44e31399fa4840954f1f7b8e14b96052e3622019c8ab4856a62f3d4926cbd645f8206d3884a3d173', 1, '2019-08-27 00:00:00', 14),
+(15, 'Miguel', '44e31399fa4840954f1f7b8e14b96052e3622019c8ab4856a62f3d4926cbd645f8206d3884a3d173', 1, '2019-08-27 00:00:00', 15);
 
 -- --------------------------------------------------------
 
@@ -180,7 +185,21 @@ CREATE TABLE `usuario_roles` (
 INSERT INTO `usuario_roles` (`idUsr`, `idRoles`, `fechaCreacion`, `creadoPor`) VALUES
 (1, 1, '2019-08-16', 'DAMIAN'),
 (1, 2, '2019-08-16', 'DAMIAN'),
-(2, 2, '2019-08-16', 'DAMIAN');
+(1, 4, '2019-08-27', 'DAMIAN'),
+(2, 2, '2019-08-16', 'DAMIAN'),
+(3, 3, '2019-08-27', 'DAMIAN'),
+(4, 3, '2019-08-27', 'DAMIAN'),
+(5, 3, '2019-08-27', 'DAMIAN'),
+(7, 3, '2019-08-27', 'DAMIAN'),
+(8, 3, '2019-08-27', 'DAMIAN'),
+(9, 3, '2019-08-27', 'DAMIAN'),
+(10, 2, '2019-08-27', 'DAMIAN'),
+(10, 3, '2019-08-27', 'DAMIAN'),
+(11, 3, '2019-08-27', 'DAMIAN'),
+(12, 3, '2019-08-27', 'DAMIAN'),
+(13, 3, '2019-08-27', 'DAMIAN'),
+(14, 3, '2019-08-27', 'DAMIAN'),
+(15, 3, '2019-08-27', 'DAMIAN');
 
 --
 -- Índices para tablas volcadas
@@ -234,25 +253,26 @@ ALTER TABLE `usuario_roles`
 --
 ALTER TABLE `admin`
   MODIFY `idAd` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
-
+--
+-- AUTO_INCREMENT de la tabla `datospersonales`
+--
+ALTER TABLE `datospersonales`
+  MODIFY `idDatosPers` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT de la tabla `direccion`
 --
 ALTER TABLE `direccion`
   MODIFY `idDir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
 --
 -- AUTO_INCREMENT de la tabla `roles`
 --
 ALTER TABLE `roles`
   MODIFY `idRoles` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idUsr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
+  MODIFY `idUsr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- Restricciones para tablas volcadas
 --
@@ -275,7 +295,6 @@ ALTER TABLE `direccion`
 ALTER TABLE `usuario_roles`
   ADD CONSTRAINT `FK_idRoles` FOREIGN KEY (`idRoles`) REFERENCES `roles` (`idRoles`),
   ADD CONSTRAINT `FK_idUsr` FOREIGN KEY (`idUsr`) REFERENCES `usuario` (`idUsr`);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
