581658cda6be548556038ffb630d096a94c800d0
damian.usheff <damian.usheff@GICA-MAD-600.grupoica.local>
2019-09-20 11:03:45 +0200

    BBDD con países en inglés

--- WebContent/WEB-INF/utils/springbd.sql
+++ WebContent/WEB-INF/utils/springbd.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-09-2019 a las 00:13:06
--- Versión del servidor: 10.1.13-MariaDB
--- Versión de PHP: 5.6.20
+-- Tiempo de generación: 20-09-2019 a las 11:02:22
+-- Versión del servidor: 10.3.16-MariaDB
+-- Versión de PHP: 7.1.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -69,7 +71,7 @@ CREATE TABLE `datospersonales` (
   `dni` varchar(15) COLLATE utf8_spanish_ci DEFAULT NULL,
   `email` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
   `telefono` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `observaciones` text COLLATE utf8_spanish_ci,
+  `observaciones` text COLLATE utf8_spanish_ci DEFAULT NULL,
   `datospersonales_idUsr` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
@@ -81,7 +83,8 @@ INSERT INTO `datospersonales` (`idDatosPers`, `nombre`, `apellido1`, `apellido2`
 (1, 'José', 'Martínez', 'Suárez', 'Hombre', '1967-07-11', 'España', '123456789A', 'pepe@pepe.com', '666555444', 'El usuario principal con el cual hacer pruebas', 1),
 (3, 'Roberto', 'Do Santos', '', 'Hombre', '1980-07-28', 'Portugal', '123456789D', 'roberto@roberto.com', '', '', 3),
 (4, 'Damián', 'Usheff', '', 'Hombre', '1976-10-30', 'Alemania', '123456789D', 'damian@damian.com', '', '', 4),
-(32, 'Damián', 'Usheff', 'Vellianitis', 'Hombre', '1976-10-30', 'Austria', '12345678A', 'damian@damian.com', '698765435', 'Observaciones para ser guardadas en base de datos', 44);
+(32, 'Damián', 'Usheff', 'Vellianitis', 'Hombre', '1976-10-30', 'Austria', '12345678A', 'damian@damian.com', '698765435', 'Observaciones para ser guardadas en base de datos', 44),
+(34, 'Prueba 19:13', 'Apellido 23:43', '', NULL, NULL, 'empty', '', '', '', '', 46);
 
 -- --------------------------------------------------------
 
@@ -111,7 +114,9 @@ INSERT INTO `direccion` (`idDir`, `tipoVia`, `nombreVia`, `numero`, `resto`, `cp
 (11, 'Calle', 'Ana de Austria', '50', NULL, '28050', 'Madrid', 'Madrid', 'España', 4),
 (12, 'Avenida', 'Niceto Alcalá Zamora', '200', 'Urbanización El sol naciente', '28050', 'Madrid', 'Madrid', 'España', 4),
 (13, 'Plaza', 'Hidalgo', '3', NULL, '99009', 'Vallladolid', 'Valladolid', 'España', 1),
-(15, 'Calle', 'Martín', '23', NULL, '28050', 'Madrid', 'Sanchinaroo', 'España', 3);
+(15, 'Calle', 'Martín', '23', NULL, '28050', 'Madrid', 'Sanchinaroo', 'España', 3),
+(16, 'Calle', '', '', '', '', '', '', 'Portugal', 32),
+(18, 'Calle', 'Américo Castro 104 2ºB', '', '', '28050', 'Madrid', 'Madrid', 'España', 34);
 
 -- --------------------------------------------------------
 
@@ -149,13 +154,13 @@ CREATE TABLE `empresa` (
   `idEmp` int(10) NOT NULL,
   `nombreComercial` varchar(50) COLLATE utf8_spanish_ci DEFAULT NULL,
   `tipoSociedad` varchar(50) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `actividad` text COLLATE utf8_spanish_ci,
+  `actividad` text COLLATE utf8_spanish_ci DEFAULT NULL,
   `cif` varchar(10) COLLATE utf8_spanish_ci DEFAULT NULL,
   `email` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
   `paginaWeb` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
   `telefono` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
   `fax` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `observaciones` text COLLATE utf8_spanish_ci
+  `observaciones` text COLLATE utf8_spanish_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
@@ -184,22 +189,22 @@ CREATE TABLE `paises` (
 INSERT INTO `paises` (`idPais`, `nombreES`, `nombreEN`) VALUES
 (1, 'España', 'Spain'),
 (2, 'Alemania', 'Germany'),
-(3, 'Andorra', ''),
-(4, 'Austria', ''),
-(5, 'Bélgica', ''),
-(6, 'Dinamarca', ''),
-(7, 'El Vaticano', ''),
-(8, 'Francia', ''),
-(9, 'Grecia', ''),
-(10, 'Italia', ''),
-(11, 'Noruega', ''),
-(12, 'Países Bajos', ''),
-(13, 'Portugal', ''),
-(14, 'Reino Unido', ''),
-(15, 'San Marino', ''),
-(16, 'Suecia', ''),
-(17, 'Suiza', ''),
-(18, 'Turquía', '');
+(3, 'Andorra', 'Andorra'),
+(4, 'Austria', 'Austria'),
+(5, 'Bélgica', 'Belgium'),
+(6, 'Dinamarca', 'Denmark'),
+(7, 'El Vaticano', 'The Vatican'),
+(8, 'Francia', 'France'),
+(9, 'Grecia', 'Greece'),
+(10, 'Italia', 'Italy'),
+(11, 'Noruega', 'Norway'),
+(12, 'Países Bajos', 'Netherlands'),
+(13, 'Portugal', 'Portugal'),
+(14, 'Reino Unido', 'United Kingdom'),
+(15, 'San Marino', 'San Marino'),
+(16, 'Suecia', 'Sweden'),
+(17, 'Suiza', 'Switzerland'),
+(18, 'Turquía', 'Turkey');
 
 -- --------------------------------------------------------
 
@@ -244,7 +249,8 @@ INSERT INTO `usuario` (`idUsr`, `usuario`, `clave`, `habilitado`, `fechaCreacion
 (1, 'pepe', '44e31399fa4840954f1f7b8e14b96052e3622019c8ab4856a62f3d4926cbd645f8206d3884a3d173', 1, '2019-09-15 16:17:35'),
 (3, 'roberto', '44e31399fa4840954f1f7b8e14b96052e3622019c8ab4856a62f3d4926cbd645f8206d3884a3d173', 1, '2019-09-15 16:17:09'),
 (4, 'Damian', '44e31399fa4840954f1f7b8e14b96052e3622019c8ab4856a62f3d4926cbd645f8206d3884a3d173', 1, '2019-09-15 16:16:47'),
-(44, 'dusheff', 'ef25ba98adf1337503ed37ba779d741bfa56ec94039aa658c5340c2fb630573b1ff38eb811bf54a4', 1, '2019-09-15 16:15:00');
+(44, 'dusheff', 'ef25ba98adf1337503ed37ba779d741bfa56ec94039aa658c5340c2fb630573b1ff38eb811bf54a4', 1, '2019-09-15 16:15:00'),
+(46, 'prueba1804', '81add68947edcb9d803eee3e9756d3c2fa7f99e2b3e0ad03b46b67922754785a679d5dc58a451cc3', 1, '2019-09-19 12:02:56');
 
 -- --------------------------------------------------------
 
@@ -289,7 +295,8 @@ INSERT INTO `usuario_rol` (`idUsr`, `idRol`, `fechaCreacion`, `creadoPor`) VALUE
 (1, 4, '2019-08-27', 'DAMIAN'),
 (3, 1, '2019-08-27', 'DAMIAN'),
 (4, 1, '2019-08-27', 'DAMIAN'),
-(44, 1, '2019-09-14', 'DAMIAN');
+(44, 1, '2019-09-14', 'DAMIAN'),
+(46, 1, '2019-09-19', 'DAMIAN');
 
 --
 -- Índices para tablas volcadas
@@ -369,41 +376,49 @@ ALTER TABLE `usuario_rol`
 --
 ALTER TABLE `admin`
   MODIFY `idAd` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
 --
 -- AUTO_INCREMENT de la tabla `datospersonales`
 --
 ALTER TABLE `datospersonales`
-  MODIFY `idDatosPers` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `idDatosPers` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
 --
 -- AUTO_INCREMENT de la tabla `direccion`
 --
 ALTER TABLE `direccion`
-  MODIFY `idDir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `idDir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
 --
 -- AUTO_INCREMENT de la tabla `direccionempresa`
 --
 ALTER TABLE `direccionempresa`
   MODIFY `idDirEmp` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT de la tabla `empresa`
 --
 ALTER TABLE `empresa`
   MODIFY `idEmp` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT de la tabla `paises`
 --
 ALTER TABLE `paises`
   MODIFY `idPais` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
 --
 -- AUTO_INCREMENT de la tabla `rol`
 --
 ALTER TABLE `rol`
   MODIFY `idRol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idUsr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `idUsr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+
 --
 -- Restricciones para tablas volcadas
 --
@@ -439,6 +454,7 @@ ALTER TABLE `usuario_empresa`
 ALTER TABLE `usuario_rol`
   ADD CONSTRAINT `FK_idRol` FOREIGN KEY (`idRol`) REFERENCES `rol` (`idRol`),
   ADD CONSTRAINT `FK_idUsr` FOREIGN KEY (`idUsr`) REFERENCES `usuario` (`idUsr`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
