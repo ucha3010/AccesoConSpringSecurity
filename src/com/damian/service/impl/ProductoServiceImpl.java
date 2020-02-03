9392f1fd90930f5f68b6efc5f7da95a50401ffec
Damian <damianjava@gmail.com>
2020-02-04 00:46:42 +0100

    Guardando facturas en cuotas

--- src/com/damian/service/impl/ProductoServiceImpl.java
+++ src/com/damian/service/impl/ProductoServiceImpl.java
@@ -135,28 +135,15 @@ public class ProductoServiceImpl implements ProductoService {
 		}
 		productoDAO.save(producto);
 
+		org.springframework.security.core.context.SecurityContextImpl context = (org.springframework.security.core.context.SecurityContextImpl) request
+				.getSession().getAttribute("SPRING_SECURITY_CONTEXT");
 		Factura factura = new Factura();
 
 		if (frontProductoStock.getCuotas() == null) {
-			factura.setCompra(frontProductoStock.isCompra());
-			factura.setIvaTotal(frontProductoStock.getIva());
-			if (frontProductoStock.getIva() > 0 && frontProductoStock.getPrecioFinal() > 0) {
-				factura.setIvaImporteTotal(((precioUnitConIva.subtract(precioUnitSinIva))
-						.multiply(new BigDecimal(frontProductoStock.getCantidad(), MathContext.DECIMAL64)))
-								.doubleValue());
-			}
-			factura.setImporteTotal(frontProductoStock.getPrecioFinal());
+
+			rellenaFacturaComun(factura, frontProductoStock, precioUnitConIva, precioUnitSinIva, context);
 			factura.setFechaCompra(new Date());
-			factura.setObservaciones(frontProductoStock.getObservaciones());
-			org.springframework.security.core.context.SecurityContextImpl context = (org.springframework.security.core.context.SecurityContextImpl) request
-					.getSession().getAttribute("SPRING_SECURITY_CONTEXT");
-			factura.setFormaPago(new FormaPago(Constantes.MOVIMIENTO_STOCK, null, null));
-			factura.setCreadoPor(context.getAuthentication().getName());
-			if (frontProductoStock.isCompra()) {
-				factura.setEstado(new Estado(Constantes.AGREGAR_STOCK, null, null));
-			} else {
-				factura.setEstado(new Estado(Constantes.QUITAR_STOCK, null, null));
-			}
+			
 			int idFac = facturaService.save(factura, request);
 			factura.setIdFac(idFac);
 			ProductoFactura productoFactura = new ProductoFactura();
@@ -192,7 +179,71 @@ public class ProductoServiceImpl implements ProductoService {
 					.subtract(new BigDecimal(frontProductoStock.getPrecioFinal(), MathContext.DECIMAL64));
 			cuota.setInteresImp(interesImp.doubleValue());
 			int idCuo = cuotaService.save(cuota);
-			// TODO DAMIAN continuar acá
+			cuota.setIdCuo(idCuo);
+
+			BigDecimal interesPor = new BigDecimal(frontProductoStock.getInteresPor(), MathContext.DECIMAL64);
+			BigDecimal uno = new BigDecimal(1, MathContext.DECIMAL64);
+			BigDecimal cien = new BigDecimal(100, MathContext.DECIMAL64);
+			
+			BigDecimal cuotaSinInteres = precioFinal.divide(new BigDecimal(frontProductoStock.getCuotas().size(), MathContext.DECIMAL64));
+			BigDecimal residuo = cuotaSinInteres.remainder(uno);
+			residuo = residuo.multiply(cien);
+			residuo = residuo.remainder(uno);
+			BigDecimal residuoRestar = residuo.multiply(new BigDecimal(0.01, MathContext.DECIMAL64));
+			cuotaSinInteres = cuotaSinInteres.subtract(residuoRestar);
+			BigDecimal residuoSumar = residuo.multiply(new BigDecimal(frontProductoStock.getCuotas().size(), MathContext.DECIMAL64));
+			BigDecimal sumarUltimaCuota = new BigDecimal(residuoSumar.longValue(), MathContext.DECIMAL64);
+			sumarUltimaCuota = sumarUltimaCuota.multiply(new BigDecimal(0.01, MathContext.DECIMAL64));
+
+			for (FrontCuota fc : frontProductoStock.getCuotas()) {
+				
+				rellenaFacturaComun(factura, frontProductoStock, precioUnitConIva, precioUnitSinIva, context);
+				factura.setFechaCompra(fc.getFechaCompra());
+				
+				factura.setCuota(cuota);
+				factura.setImporteCuotaTotal(fc.getImporteTotal());
+				factura.setNumeroCuota(fc.getNumeroCuota());
+				
+				BigDecimal importeCuotaTotal = new BigDecimal(fc.getImporteTotal(), MathContext.DECIMAL64);
+				BigDecimal interesCuotaImporte = new BigDecimal(0, MathContext.DECIMAL64);
+				if(fc.getNumeroCuota() == 1) {
+					interesCuotaImporte = importeCuotaTotal.subtract(comisionAperturaImp).subtract(cuotaSinInteres);
+				} else if(fc.getNumeroCuota() == frontProductoStock.getCuotas().size()) {
+					interesCuotaImporte = importeCuotaTotal.subtract(cuotaSinInteres).add(sumarUltimaCuota);
+				} else {
+					interesCuotaImporte = importeCuotaTotal.subtract(cuotaSinInteres);
+				}
+				factura.setInteresCuotaImporte(interesCuotaImporte.doubleValue());
+				
+				//TODO DAMIAN falta agregar (pojo incluido) cuotaConIva y cuotaSinIva y todo productoFactura
+				
+				
+				int idFac = facturaService.save(factura, request);
+				factura.setIdFac(idFac);
+			}
+		}
+
+	}
+
+	private void rellenaFacturaComun(Factura factura, FrontProductoStock frontProductoStock,
+			BigDecimal precioUnitConIva, BigDecimal precioUnitSinIva,
+			org.springframework.security.core.context.SecurityContextImpl context) {
+
+		factura = new Factura();
+		factura.setCompra(frontProductoStock.isCompra());
+		factura.setIvaTotal(frontProductoStock.getIva());
+		if (frontProductoStock.getIva() > 0 && frontProductoStock.getPrecioFinal() > 0) {
+			factura.setIvaImporteTotal(((precioUnitConIva.subtract(precioUnitSinIva))
+					.multiply(new BigDecimal(frontProductoStock.getCantidad(), MathContext.DECIMAL64))).doubleValue());
+		}
+		factura.setImporteTotal(frontProductoStock.getPrecioFinal());
+		factura.setObservaciones(frontProductoStock.getObservaciones());
+		factura.setFormaPago(new FormaPago(Constantes.MOVIMIENTO_STOCK, null, null));
+		factura.setCreadoPor(context.getAuthentication().getName());
+		if (frontProductoStock.isCompra()) {
+			factura.setEstado(new Estado(Constantes.AGREGAR_STOCK, null, null));
+		} else {
+			factura.setEstado(new Estado(Constantes.QUITAR_STOCK, null, null));
 		}
 
 	}
--- src/test/java/TestUsuarioEmpresa.java
+++ src/test/java/TestUsuarioEmpresa.java
@@ -1,13 +1,37 @@
 package test.java;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+
 import com.damian.service.UsuarioEmpresaService;
 import com.damian.service.impl.UsuarioEmpresaServiceImpl;
 
 public class TestUsuarioEmpresa {
 
 	public static void main(String[] args) {
-		UsuarioEmpresaService ues = new UsuarioEmpresaServiceImpl();
-		ues.delete(58, 2);
+		BigDecimal cien = new BigDecimal(100.25, MathContext.DECIMAL64);
+		BigDecimal veinte = new BigDecimal(30, MathContext.DECIMAL64);
+		System.out.println(cien.remainder(veinte));
+		
+		double num = 371.25;
+	      double div = 1;
+	      double modVal;
+
+	      // Usual mod
+	      modVal = num%div;
+	       System.out.println(modVal);
+	     
+	      //Using big decimal
+	      BigDecimal bd = new BigDecimal(num);
+	      modVal = bd.remainder(new BigDecimal(div)).doubleValue();
+	      System.out.println(modVal);
+	      
+	      BigDecimal number = new BigDecimal("31.54");
+	      long iPart = number.longValue();
+	      BigDecimal fraccion = number.remainder(BigDecimal.ONE);
+
+	      System.out.println("Integer part = " + iPart);
+	      System.out.println("Fractional part = " + fraccion);
 	}
 
 }
