f78b017d6b7ae3ba796f2711a09dc4a1d3d84322
Damian <damianjava@gmail.com>
2020-12-04 02:03:09 +0100

    Reparación login desde página distinta a index

--- src/com/damian/controller/FooterController.java
+++ src/com/damian/controller/FooterController.java
@@ -29,10 +29,20 @@ public class FooterController {
 		return fillModelAndView("aboutus", "footeraboutus", modelAndView);
 	}
 
+	@RequestMapping("/private/aboutus")
+	public ModelAndView getPrivateAboutus(ModelAndView modelAndView) throws Exception {
+		return getaboutus(modelAndView);
+	}
+
 	@RequestMapping("/footer/findyourstore")
 	public ModelAndView getfindyourstore(ModelAndView modelAndView) throws Exception {
 		modelAndView.addObject("empresaPropias", empresaPropiaService.findAll());
 		return fillModelAndView("findyourstore", "footerfindyourstore", modelAndView);
+	}
+
+	@RequestMapping("/private/findyourstore")
+	public ModelAndView getPrivateFindyourstore(ModelAndView modelAndView) throws Exception {
+		return getfindyourstore(modelAndView);
 	}
 
 	@RequestMapping("/footer/workwithus")
@@ -40,9 +50,19 @@ public class FooterController {
 		return fillModelAndView("workwithus", "footerworkwithus", modelAndView);
 	}
 
+	@RequestMapping("/private/workwithus")
+	public ModelAndView getPrivateWorkwithus(ModelAndView modelAndView) throws Exception {
+		return getworkwithus(modelAndView);
+	}
+
 	@RequestMapping("/footer/legal")
 	public ModelAndView getlegal(ModelAndView modelAndView) throws Exception {
 		return fillModelAndView("legal", "footerlegal", modelAndView);
+	}
+
+	@RequestMapping("/private/legal")
+	public ModelAndView getPrivateLegal(ModelAndView modelAndView) throws Exception {
+		return getlegal(modelAndView);
 	}
 
 	@RequestMapping("/footer/customerreviews")
@@ -50,9 +70,19 @@ public class FooterController {
 		return fillModelAndView("customerreviews", "footercustomerreviews", modelAndView);
 	}
 
+	@RequestMapping("/private/customerreviews")
+	public ModelAndView getPrivateCustomerreviews(ModelAndView modelAndView) throws Exception {
+		return getcustomerreviews(modelAndView);
+	}
+
 	@RequestMapping("/footer/howtobuy")
 	public ModelAndView gethowtobuy(ModelAndView modelAndView) throws Exception {
 		return fillModelAndView("howtobuy", "footerhowtobuy", modelAndView);
+	}
+
+	@RequestMapping("/private/howtobuy")
+	public ModelAndView getPrivateHowtobuy(ModelAndView modelAndView) throws Exception {
+		return gethowtobuy(modelAndView);
 	}
 
 	@RequestMapping("/footer/cookiespolicy")
@@ -60,14 +90,29 @@ public class FooterController {
 		return fillModelAndView("cookiespolicy", "footercookiespolicy", modelAndView);
 	}
 
+	@RequestMapping("/private/cookiespolicy")
+	public ModelAndView getPrivateCookiespolicy(ModelAndView modelAndView) throws Exception {
+		return getcookiespolicy(modelAndView);
+	}
+
 	@RequestMapping("/footer/shipments")
 	public ModelAndView getshipments(ModelAndView modelAndView) throws Exception {
 		return fillModelAndView("shipments", "footershipments", modelAndView);
 	}
 
+	@RequestMapping("/private/shipments")
+	public ModelAndView getPrivateShipments(ModelAndView modelAndView) throws Exception {
+		return getshipments(modelAndView);
+	}
+
 	@RequestMapping("/footer/giftcard")
 	public ModelAndView getgiftcard(ModelAndView modelAndView) throws Exception {
 		return fillModelAndView("giftcard", "footergiftcard", modelAndView);
+	}
+
+	@RequestMapping("/private/giftcard")
+	public ModelAndView getPrivateGiftcard(ModelAndView modelAndView) throws Exception {
+		return getgiftcard(modelAndView);
 	}
 
 	@RequestMapping("/footer/brands")
@@ -76,9 +121,19 @@ public class FooterController {
 		return fillModelAndView("brands", "footerbrands", modelAndView);
 	}
 
+	@RequestMapping("/private/brands")
+	public ModelAndView getPrivateBrands(ModelAndView modelAndView) throws Exception {
+		return getbrands(modelAndView);
+	}
+
 	@RequestMapping("/footer/membersclub")
 	public ModelAndView getmembersclub(ModelAndView modelAndView) throws Exception {
 		return fillModelAndView("membersclub", "footermembersclub", modelAndView);
+	}
+
+	@RequestMapping("/private/membersclub")
+	public ModelAndView getPrivateMembersclub(ModelAndView modelAndView) throws Exception {
+		return getmembersclub(modelAndView);
 	}
 
 	@RequestMapping("/footer/contact")
@@ -86,9 +141,19 @@ public class FooterController {
 		return fillModelAndView("contact", "footercontact", modelAndView);
 	}
 
+	@RequestMapping("/private/contact")
+	public ModelAndView getPrivateContact(ModelAndView modelAndView) throws Exception {
+		return getcontact(modelAndView);
+	}
+
 	@RequestMapping("/footer/Frequentquestions")
 	public ModelAndView getFrequentquestions(ModelAndView modelAndView) throws Exception {
 		return fillModelAndView("Frequentquestions", "footerFrequentquestions", modelAndView);
+	}
+
+	@RequestMapping("/private/Frequentquestions")
+	public ModelAndView getPrivateFrequentquestions(ModelAndView modelAndView) throws Exception {
+		return getFrequentquestions(modelAndView);
 	}
 
 	@RequestMapping("/footer/refund")
@@ -96,9 +161,19 @@ public class FooterController {
 		return fillModelAndView("refund", "footerrefund", modelAndView);
 	}
 
+	@RequestMapping("/private/refund")
+	public ModelAndView getPrivateRefund(ModelAndView modelAndView) throws Exception {
+		return getrefund(modelAndView);
+	}
+
 	@RequestMapping("/footer/guarantee")
 	public ModelAndView getguarantee(ModelAndView modelAndView) throws Exception {
 		return fillModelAndView("guarantee", "footerguarantee", modelAndView);
+	}
+
+	@RequestMapping("/private/guarantee")
+	public ModelAndView getPrivateGuarantee(ModelAndView modelAndView) throws Exception {
+		return getguarantee(modelAndView);
 	}
 
 	@RequestMapping("/footer/promotion1")
@@ -106,9 +181,19 @@ public class FooterController {
 		return fillModelAndView("promotion1", "promotion1", modelAndView);
 	}
 
+	@RequestMapping("/private/promotion1")
+	public ModelAndView getPrivatePromotion1(ModelAndView modelAndView) throws Exception {
+		return getpromotion1(modelAndView);
+	}
+
 	@RequestMapping("/footer/promotion2")
 	public ModelAndView getpromotion2(ModelAndView modelAndView) throws Exception {
 		return fillModelAndView("promotion2", "promotion2", modelAndView);
+	}
+
+	@RequestMapping("/private/promotion2")
+	public ModelAndView getPrivatePromotion2(ModelAndView modelAndView) throws Exception {
+		return getpromotion2(modelAndView);
 	}
 
 	@RequestMapping("/footer/promotion3")
@@ -116,9 +201,19 @@ public class FooterController {
 		return fillModelAndView("promotion3", "promotion3", modelAndView);
 	}
 
+	@RequestMapping("/private/promotion3")
+	public ModelAndView getPrivatePromotion3(ModelAndView modelAndView) throws Exception {
+		return getpromotion3(modelAndView);
+	}
+
 	@RequestMapping("/footer/promotion4")
 	public ModelAndView getpromotion4(ModelAndView modelAndView) throws Exception {
 		return fillModelAndView("promotion4", "promotion4", modelAndView);
+	}
+
+	@RequestMapping("/private/promotion4")
+	public ModelAndView getPrivatePromotion4(ModelAndView modelAndView) throws Exception {
+		return getpromotion4(modelAndView);
 	}
 
 	private ModelAndView fillModelAndView(String estoy, String go, ModelAndView modelAndView) {
